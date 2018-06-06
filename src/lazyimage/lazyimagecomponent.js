9d635c9ee525c1785583523bde1fc7cfd7f80b97
Steven Le <stevenle08@gmail.com>
2018-06-06 12:10:54 -0700

    Ignore lazyimage loads when el is `display: none`

--- src/lazyimage/lazyimagecomponent.js
+++ src/lazyimage/lazyimagecomponent.js
@@ -47,34 +47,60 @@ export class LazyImageComponent extends InviewComponent {
     if (this.element.hasAttribute('lazyimage-src')) {
       imgEls.push(this.element);
     }
-    imgEls.forEach((el) => {
-      const src = el.getAttribute('lazyimage-src');
-      this.imageLoader.load(src).then((loadedSrc) => {
-        el.src = loadedSrc;
-        el.setAttribute('lazyimage-loaded', 'true');
-      });
-    });
+    imgEls
+        .filter((el) => this.isElementVisisble_(el) && !this.isImageLoaded_(el))
+        .forEach((el) => {
+          const src = el.getAttribute('lazyimage-src');
+          this.imageLoader.load(src).then((loadedSrc) => {
+            el.src = loadedSrc;
+            el.setAttribute('lazyimage-loaded', 'true');
+          });
+        });
 
     const bgEls = [].slice.call(
         this.element.querySelectorAll('[lazyimage-background-image]'));
     if (this.element.hasAttribute('lazyimage-background-image')) {
       bgEls.push(this.element);
     }
-    bgEls.forEach((el) => {
-      const src = el.getAttribute('lazyimage-background-image');
-      this.imageLoader.load(src).then((loadedSrc) => {
-        el.style.backgroundImage = `url(${loadedSrc})`;
-        el.setAttribute('lazyimage-loaded', 'true');
-      });
-    });
+    bgEls
+        .filter((el) => this.isElementVisisble_(el) && !this.isImageLoaded_(el))
+        .forEach((el) => {
+          const src = el.getAttribute('lazyimage-background-image');
+          this.imageLoader.load(src).then((loadedSrc) => {
+            el.style.backgroundImage = `url(${loadedSrc})`;
+            el.setAttribute('lazyimage-loaded', 'true');
+          });
+        });
 
     this.imageLoader.wait().then(() => {
       this.loading = false;
-      this.loaded = true;
+      // TODO(stevenle): figure out a good way to deterministically tell if all
+      // images have been loaded when some images may have `display: none`.
+      // this.loaded = true;
       if (this.options.loadedClass) {
         this.element.classList.add(this.options.loadedClass);
       }
     });
+  }
+
+  /**
+   * Returns `true` if the image is loaded.
+   * @param {Element} element
+   * @return {boolean}
+   * @private
+   */
+  isImageLoaded_(element) {
+    return element.getAttribute('lazyimage-loaded') === 'true';
+  }
+
+  /**
+   * Returns `true` if the element isn't `display: none`.
+   * @param {Element} element
+   * @return {boolean}
+   * @private
+   */
+  isElementVisisble_(element) {
+    return element.offsetParent !== null;
   }
 }
 
